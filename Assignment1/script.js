4d5d2b836dd74fcb3b69153240080cabbc83ef6e
Anisat Akinbani <52763841+Annysah@users.noreply.github.com>
2023-08-26 16:16:32 -0500

    Update repo

---
+++ Assignment1/script.js
@@ -0,0 +1,10 @@
+const playButton = document.getElementById('play-button');
+const audioPlayer = document.getElementById('audio-player');
+
+playButton.addEventListener('click', () => {
+    if (audioPlayer.paused) {
+        audioPlayer.play();
+    } else {
+        audioPlayer.pause();
+    }
+});
